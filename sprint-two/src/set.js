13e324b9325ef84ae36d025addfcde6bc37ec395
Juan Marin <benjamin.v.stone@gmail.com>
2014-02-27 11:32:07 -0800

    Finished set with JSON.stringify

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,16 +1,20 @@
 var makeSet = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  this._storage[JSON.stringify(item)] = item;
+  return this._storage[JSON.stringify(item)];
 };
 
 setPrototype.contains = function(item){
+  return this._storage[JSON.stringify(item)] ? true : false;
 };
 
 setPrototype.remove = function(item){
+  delete this._storage[JSON.stringify(item)];
 };
