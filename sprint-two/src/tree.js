eeb17a3d6d4b5410bc3d3c614afac014be42eaa2
Hack Reactor Students <nicholas@niceguys.co>
2014-03-01 10:52:06 -0800

    Finished tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -22,7 +22,17 @@ var treeMethods = {
     this.children.push(makeTree(value, this));
   },
   contains : function(target){
+    var flag = false;
+    //If this node's value = target, return true
+    if(this.value === target) {
+      return true;
+    }
+    //Otherwise, test each of the children
+    _.each(this.children, function(child) {
+      flag = flag || child.contains(target);
+    });
 
+    return flag;
   },
 };
 
