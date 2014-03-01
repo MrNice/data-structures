697e2cb881f4f6816f93d5f92c49f10153929166
Hack Reactor Students <nicholas@niceguys.co>
2014-03-01 11:46:15 -0800

    Pseudocode skeletoned Hashtable, touched tree

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -2,15 +2,42 @@ var HashTable = function(){
   this._limit = 8;
   this._storage = makeLimitedArray(this._limit);
 };
+/*********************************************
+table.set(getIndexBelowMaxForKey("Sup", 8), ["Sup", "Dawg"]);
+undefined
+table.get(getIndexBelowMaxForKey("Sup", 8));
+*********************************************/
+
+
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  //If storage[i] is empty, set it to [[k, v]]
+
+  //Otherwise, search storage[i] for arr[0] === key
+
+  //if found, set arr[1] to value
+
+  //otherwise, storage[i].push([k, v])
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  //If storage[i] is empty, return undefined
+
+  //Else, search storage[i] for arr[0] === key
+
+  //if found, return value
+
+  //else, return undefined
 };
 
 HashTable.prototype.remove = function(k){
-};
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  //If storage[i] is empty, return undefined
 
+  //Else, search storage[i] for arr[0] === key
+
+  //If found, splice it out of the array.
+  // NOTE: use splice to modify the array.
+};
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -16,18 +16,18 @@ var makeTree = function(value, parent, children){
   return newTree;
 };
 
-
+// TODO: refactor to use 'find'
 var treeMethods = {
   addChild : function(value){
     this.children.push(makeTree(value, this));
   },
   contains : function(target){
     var flag = false;
-    //If this node's value = target, return true
+
     if(this.value === target) {
       return true;
     }
-    //Otherwise, test each of the children
+
     _.each(this.children, function(child) {
       flag = flag || child.contains(target);
     });
